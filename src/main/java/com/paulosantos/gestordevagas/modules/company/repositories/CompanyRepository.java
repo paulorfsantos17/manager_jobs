864328a670d74bd073555a14c4610901ee03e1e3
Paulo <paulorfsantos17@gmail.com>
2024-05-14 18:07:02 -0300

    feat: add CompanyRepository

---
+++ src/main/java/com/paulosantos/gestordevagas/modules/company/repositories/CompanyRepository.java
@@ -0,0 +1,12 @@
+package com.paulosantos.gestordevagas.modules.company.repositories;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+import com.paulosantos.gestordevagas.modules.company.entities.CompanyEntity;
+
+public interface CompanyRepository extends JpaRepository<CompanyEntity, UUID> {
+  Optional<CompanyEntity> findByUsernameOrEmail(String username, String email);
+
+}
