50e374ace04b724daddb027bbf44ce2772e9e666
Paulo <paulorfsantos17@gmail.com>
2024-05-14 18:50:04 -0300

    Fea: add JobEntity

--- src/main/java/com/paulosantos/gestordevagas/modules/company/entities/JobEntity.java
+++ src/main/java/com/paulosantos/gestordevagas/modules/company/entities/JobEntity.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import java.util.UUID;
 import java.time.LocalDateTime;
@@ -22,13 +23,15 @@ public class JobEntity {
   private UUID id;
   private String description;
   private String benefits;
+
+  @NotBlank(message = "Campo [nível] é obrigatório. ")
   private String level;
 
   @ManyToOne()
   @JoinColumn(name = "company_id", insertable = false, updatable = false)
   private CompanyEntity companyEntity;
 
-  @Column(name = "company_id")
+  @Column(name = "company_id", nullable = false)
   private UUID companyId;
 
   @CreationTimestamp
