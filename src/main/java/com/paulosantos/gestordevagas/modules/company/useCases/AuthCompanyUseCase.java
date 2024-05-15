e0601b765cdaa6d5c112459b91049f4faf31c0df
Paulo <paulorfsantos17@gmail.com>
2024-05-15 15:00:09 -0300

    feat add AuthCompanyUseCase

--- src/main/java/com/paulosantos/gestordevagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/com/paulosantos/gestordevagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -1,8 +1,48 @@
 package com.paulosantos.gestordevagas.modules.company.useCases;
 
+import javax.naming.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.paulosantos.gestordevagas.exceptions.CompanyNotFoundException;
+import com.paulosantos.gestordevagas.modules.company.dto.AuthCompanyDTO;
+import com.paulosantos.gestordevagas.modules.company.entities.CompanyEntity;
+import com.paulosantos.gestordevagas.modules.company.repositories.CompanyRepository;
 
 @Service
 public class AuthCompanyUseCase {
 
+  @Value("${security.token.secret}")
+  private String secretKey;
+
+  @Autowired
+  private CompanyRepository companyRepository;
+
+  @Autowired
+  private PasswordEncoder passwordEncode;
+
+  public void execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+    CompanyEntity company = this.companyRepository.findByUsername(authCompanyDTO.getUsername())
+        .orElseThrow(() -> {
+          throw new CompanyNotFoundException();
+        });
+
+    boolean passwordMatches = this.passwordEncode.matches(authCompanyDTO.getPassword(), company.getPassword());
+
+    if (!passwordMatches) {
+      throw new AuthenticationException();
+    }
+
+    Algorithm algorithm = Algorithm.HMAC256(secretKey);
+    String token = JWT.create().withIssuer("javagas")
+        .withSubject(company.getId().toString())
+        .sign(algorithm);
+  }
+
+  return token
 }
