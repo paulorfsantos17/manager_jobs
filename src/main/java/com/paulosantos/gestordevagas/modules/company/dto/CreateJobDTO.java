a393ddc4c2b6fa5b978a481ca96944956042d8f1
Paulo <paulorfsantos17@gmail.com>
2024-05-15 17:43:52 -0300

    feat: add CreateJobDTO

---
+++ src/main/java/com/paulosantos/gestordevagas/modules/company/dto/CreateJobDTO.java
@@ -0,0 +1,10 @@
+package com.paulosantos.gestordevagas.modules.company.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateJobDTO {
+  private String description;
+  private String benefits;
+  private String level;
+}
