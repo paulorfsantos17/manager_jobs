48af0729c33435873c422a1e0f51b82ce8db82c2
Paulo <paulorfsantos17@gmail.com>
2024-05-15 14:59:43 -0300

    feat: add method findByUserName in CompanyRepository

--- src/main/java/com/paulosantos/gestordevagas/modules/company/repositories/CompanyRepository.java
+++ src/main/java/com/paulosantos/gestordevagas/modules/company/repositories/CompanyRepository.java
@@ -9,4 +9,6 @@ import com.paulosantos.gestordevagas.modules.company.entities.CompanyEntity;
 public interface CompanyRepository extends JpaRepository<CompanyEntity, UUID> {
   Optional<CompanyEntity> findByUsernameOrEmail(String username, String email);
 
+  Optional<CompanyEntity> findByUsername(String username);
+
 }
