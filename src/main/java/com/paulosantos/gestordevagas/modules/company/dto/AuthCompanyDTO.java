e1a79487f13b3dc37ee7b64caab3f5a428187310
Paulo <paulorfsantos17@gmail.com>
2024-05-15 14:58:17 -0300

    feat: add AuthCompanyDTO

---
+++ src/main/java/com/paulosantos/gestordevagas/modules/company/dto/AuthCompanyDTO.java
@@ -0,0 +1,13 @@
+package com.paulosantos.gestordevagas.modules.company.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthCompanyDTO {
+
+  private String password;
+  private String username;
+
+}
