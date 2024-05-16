518f0c7cbda02b665fa92bb68b50154e57984815
Paulo <paulorfsantos17@gmail.com>
2024-05-16 14:57:02 -0300

    refactor: routes order by and add filter candidate.

--- src/main/java/com/paulosantos/gestordevagas/security/SecurityConfig.java
+++ src/main/java/com/paulosantos/gestordevagas/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.paulosantos.gestordevagas.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,21 +11,27 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
   @Autowired
   private SecurityFilter securityFilter;
+
+  @Autowired
+  SecurityCandidateFilter securityCandidateFilter;
 
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> {
       auth.requestMatchers("/candidate/").permitAll()
           .requestMatchers("/company/").permitAll()
-          .requestMatchers("/auth/company").permitAll()
-          .requestMatchers("/auth/candidate").permitAll();
+          .requestMatchers("/company/auth").permitAll()
+          .requestMatchers("/candidate/auth").permitAll();
       auth.anyRequest().authenticated();
 
-    }).addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
+    }).addFilterBefore(securityCandidateFilter, BasicAuthenticationFilter.class)
+        .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
+
     return http.build();
   }
 
