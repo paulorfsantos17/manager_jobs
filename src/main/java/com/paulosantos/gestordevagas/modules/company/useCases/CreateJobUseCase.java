d82308309049cc27702d78c7879accefdf1256ad
Paulo <paulorfsantos17@gmail.com>
2024-06-05 14:25:43 -0300

    feat : add validation candidate not found

--- src/main/java/com/paulosantos/gestordevagas/modules/company/useCases/CreateJobUseCase.java
+++ src/main/java/com/paulosantos/gestordevagas/modules/company/useCases/CreateJobUseCase.java
@@ -3,7 +3,9 @@ package com.paulosantos.gestordevagas.modules.company.useCases;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.paulosantos.gestordevagas.exceptions.CompanyNotFoundException;
 import com.paulosantos.gestordevagas.modules.company.entities.JobEntity;
+import com.paulosantos.gestordevagas.modules.company.repositories.CompanyRepository;
 import com.paulosantos.gestordevagas.modules.company.repositories.JobRepository;
 
 @Service
@@ -12,7 +14,14 @@ public class CreateJobUseCase {
   @Autowired
   private JobRepository jobRepository;
 
+  @Autowired
+  private CompanyRepository companyRepository;
+
   public JobEntity execute(JobEntity jobEntity) {
+    companyRepository.findById(jobEntity.getCompanyId()).orElseThrow(() -> {
+      throw new CompanyNotFoundException();
+    });
+
     return jobRepository.save(jobEntity);
   }
 }
